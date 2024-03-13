31ba2c9051e8736365245c7e2d316f2e388a2eb7
Jonathan Allen <jallen@snow.edu>
2024-03-13 10:13:10 -0600

    renamed projects

---
+++ MultiplayerMover.Logic/DisplayImage.cs
@@ -0,0 +1,61 @@
+﻿namespace MultiplayerMover.Logic;
+
+public class DisplayImage
+{
+    public string Url{get;set;}
+    public int Left{get;set;}
+    public int Top{get;set;}
+}
+
+public class DisplayImageWithCaption : DisplayImage
+{
+    public string Caption{get;set;}
+}
+
+public static class DefaultImages
+{
+    public static List<DisplayImage> Images=[
+        new DisplayImage{Url="https://via.placeholder.com/50"},
+        new DisplayImageWithCaption{Url="https://via.placeholder.com/25", Caption="Subclass", Left=50, Top=50},
+        new DisplayImageWithCaption{Url="https://via.placeholder.com/150", Caption="Subclass", Left=250, Top=15},
+    ];
+
+    public static List<Shape> Shapes = [
+        new Rectangle((45,52), 250, 95),
+        new Square((150,150), 130)
+    ];
+
+}
+
+public class Shape
+{
+    public int Left{get;set;}
+    public int Top{get;set;}
+}
+
+public class Rectangle : Shape
+{
+    public Rectangle((int top, int left) origin, int length, int width)
+    {
+        Left = origin.left;
+        Top = origin.top;
+        Length = length;
+        Width = width;
+    }
+    public int Length{get;set;}
+    public int Width{get;set;}
+}
+
+public class Square : Rectangle
+{
+    public Square((int top, int left) origin, int length) : base(origin, length, length)
+    {
+
+    }
+    public int Length{get;set;}
+    public new int Width
+    {
+        get => Length;
+        set => Length = value;
+    }
+}
---
+++ MultiplayerMover.Logic/GameState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+
+namespace MultiplayerMover.Logic;
+
+public static class GameState
+{
+    public static PlayerState JoinPlayer(string playerName)
+    {
+        PlayerState playerState = new PlayerState(playerName);
+        GameState.playerState.TryAdd(playerName, playerState);
+        RaisePlayerStateChanged();
+        return playerState;
+    }
+
+    private static ConcurrentDictionary<string, PlayerState> playerState = new();
+
+    public static event EventHandler<IEnumerable<(string playerName, PlayerState state)>>? PlayerStateChanged;
+    public static void RaisePlayerStateChanged()
+    {
+        Players = playerState.Values;
+        PlayerStateChanged?.Invoke(null, playerState.Select(kvp => (kvp.Key, kvp.Value)));
+    }
+    public static IEnumerable<PlayerState> Players { get; private set; }
+}
+
+public class PlayerState(string playerName)
+{
+    public string Name => playerName;
+    public int X { get; set; }
+    public int Y { get; set; }
+}
